82a234ab2d6f7efcbdc1d9332bef3bff231b53d8
Sibgatullina Emilia <isip_e.e.sibgatullina@mpt.ru>
2022-09-06 15:13:16 +0300

    first commit

---
+++ src/main/java/com/example/Second_PR/controllers/OwnerController.java
@@ -0,0 +1,57 @@
+package com.example.Second_PR.controllers;
+
+import com.example.Second_PR.models.Owner;
+import com.example.Second_PR.repo.OwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class OwnerController {
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @GetMapping("/owner")
+    public String ownerMain(Model model)
+    {
+        Iterable<Owner> owners = ownerRepository.findAll();
+        model.addAttribute("owners", owners);
+        return "owner/owner-main";
+    }
+    @GetMapping("/owner/add")
+    public String ownerAdd(Model model)
+    {
+        return "owner/owner-add";
+    }
+    @PostMapping("/owner/add")
+    public String ownerDataAdd(
+            @RequestParam String name,
+            @RequestParam String post,
+            @RequestParam Integer age,
+            @RequestParam Character sex,
+            @RequestParam double salary, Model model)
+    {
+        Owner owner = new Owner(name, post, age, sex, salary);
+        ownerRepository.save(owner);
+        return "redirect:/owner";
+    }
+
+    @GetMapping("/owner/filter")
+    public String ownerFilter(Model model)
+    {
+        return "owner/owner-filter";
+    }
+
+    @PostMapping("/owner/filter/result")
+    public String ownerResult(@RequestParam String name, Model model)
+    {
+        List<Owner> result = ownerRepository.findByNameContains(name);
+        model.addAttribute("result", result);
+        return "owner/owner-filter";
+    }
+}
